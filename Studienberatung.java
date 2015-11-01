806aa2d6ee9327bdecfbf9c2a84ed1730c415f6c
Your Name <you@example.com>
2015-11-01 14:01:11 +0100

    initialized Scanner

---
+++ Studienberatung.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Studienberatung{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		String answer = scanner.next();
+	}
+	
+}
