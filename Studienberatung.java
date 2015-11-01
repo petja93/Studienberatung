a7b02d316738b1624221533297bdcfd278d8b25e
Your Name <you@example.com>
2015-11-01 14:13:49 +0100

    bist-du-wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -6,6 +6,15 @@ public class Studienberatung{
 		
 		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
+		if(answer.equalsIgnoreCase("j")){
+			System.out.println("Bist du wissbegierig?");
+			answer = scanner.next();
+			if(answer.equalsIgnoreCase("j")){
+				System.out.println("Auf jeden Fall studieren");
+			}else if(answer.equalsIgnoreCase("n")){
+				System.out.println("Nicht studieren");
+			}
+		}
 	}
 	
 }
