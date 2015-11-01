3b82ace6d8107dd8dc461e80273bbca626ea16ab
Your Name <you@example.com>
2015-11-01 14:31:59 +0100

    hast-du-eine-berufsausbildung

--- Studienberatung.java
+++ Studienberatung.java
@@ -11,8 +11,15 @@ public class Studienberatung{
 			answer = scanner.next();
 			if(answer.equalsIgnoreCase("n")){
 				System.out.println("Nicht studieren");
+			}else if(answer.equalsIgnoreCase("j")){
+				System.out.println("Hast du eine Berufsausbildung?");
+				answer = scanner.next();
+				if(answer.equalsIgnoreCase("j")){
+					System.out.println("Du kannst ja spaeter noch studieren");
+				}else if(answer.equalsIgnoreCase("n")){
+					System.out.println("Besser noch studieren");
+				}
 			}
 		}
 	}
-	
 }
